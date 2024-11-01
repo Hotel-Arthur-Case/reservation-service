5078f23fb18a9f8bef56f437c12830cf5010e154
BirkLauritzen <birklauritzen@gmail.com>
2024-11-01 10:44:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def export_reservations_csv():
     return output
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
